cced57f3053bf507d0169acadf6a68e37f58e09f
cwkim723 <jerry.pref@gmail.com>
2022-03-08 14:17:52 +0900

    [프로그래머스] 최소직사각형

---
+++ programmers/최소직사각형.java
@@ -0,0 +1,32 @@
+package String;
+
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[][] sizes) {
+        int answer = 0;
+        int width = 0;
+        int height = 0;
+        int temp = 0;
+
+        for(int i = 0; i < sizes.length; i++){
+            if(sizes[i][0] < sizes[i][1]){
+                temp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = temp;
+            }
+
+            if(width <= sizes[i][0]){
+                width = sizes[i][0];
+            };
+            if(height <= sizes[i][1]){
+                height = sizes[i][1];
+            };
+        }
+
+        answer = width * height;
+
+
+        return answer;
+    }
+}
