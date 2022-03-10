594047f8314ffa5d250bafd4297ed8fd6501086f
cwkim723 <jerry.pref@gmail.com>
2022-03-11 00:10:26 +0900

    [백준 2439] 별 찍기 - 2

---
+++ baekjoon/for문/2439.java
@@ -0,0 +1,24 @@
+package String;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int t = Integer.parseInt(br.readLine());
+        for(int i = 1; i <= t; i++){
+            for(int j = 1; j <= t; j++){
+                if(j <= t-i){
+                    System.out.print(" ");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
