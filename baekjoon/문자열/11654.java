a3bcb154d13048a64b3419ea210d0f9455ad7236
cwkim723 <jerry.pref@gmail.com>
2022-03-15 20:02:59 +0900

    [백준 11654] 아스키 코드

---
+++ baekjoon/문자열/11654.java
@@ -0,0 +1,15 @@
+package String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char n = br.readLine().charAt(0);
+        int ascii = (int)n;
+
+        System.out.println(ascii);
+    }
+}
