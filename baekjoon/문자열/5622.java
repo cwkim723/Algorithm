80a284a5c84ef8de07157e0838251a7056b2292a
cwkim723 <jerry.pref@gmail.com>
2022-03-17 00:35:15 +0900

    refactor: [백준 5622] 다이얼

--- baekjoon/문자열/5622.java
+++ baekjoon/문자열/5622.java
@@ -16,21 +16,21 @@ public class Main {
         }
 
         for(int i = 0; i < arr.length; i++){
-            if((int)arr[i] >= 65 && (int)arr[i] <= 67){
+            if((int)arr[i] <= 67){
                 time += 3;
-            } else if((int)arr[i] >= 68 && (int)arr[i] <= 70){
+            } else if((int)arr[i] <= 70){
                 time += 4;
-            } else if((int)arr[i] >= 71 && (int)arr[i] <= 73){
+            } else if((int)arr[i] <= 73){
                 time += 5;
-            } else if((int)arr[i] >= 74 && (int)arr[i] <= 76){
+            } else if((int)arr[i] <= 76){
                 time += 6;
-            } else if((int)arr[i] >= 77 && (int)arr[i] <= 79){
+            } else if((int)arr[i] <= 79){
                 time += 7;
-            } else if((int)arr[i] >= 80 && (int)arr[i] <= 83){
+            } else if((int)arr[i] <= 83){
                 time += 8;
-            } else if((int)arr[i] >= 84 && (int)arr[i] <= 86){
+            } else if((int)arr[i] <= 86){
                 time += 9;
-            } else if((int)arr[i] >= 87 && (int)arr[i] <= 90){
+            } else if((int)arr[i] <= 90){
                 time += 10;
             } else {
                 time += 1;
