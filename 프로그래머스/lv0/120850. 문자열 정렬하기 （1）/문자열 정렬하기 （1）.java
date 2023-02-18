abee2366d6d37bcc56e7a95334eb3e4b252ae966
Chaewon Kim <93105083+cwkim723@users.noreply.github.com>
2023-02-18 23:38:56 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 4.78 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -1,17 +1,10 @@
 import java.util.*;
 
 class Solution {
-    public List<Integer> solution(String my_string) {
-        List<Integer> list = new ArrayList<>();
-        char strArr[] = my_string.toCharArray();
-        Arrays.sort(strArr);
-        
-        for(char str : strArr) {
-            if(Character.isDigit(str)) {
-                list.add(Character.getNumericValue(str));
-            }
-        }
-        
-        return list;
+    public int[] solution(String my_string) {
+        return Arrays.stream(my_string.replaceAll("[A-Z|a-z]", "").split(""))
+            .sorted()
+            .mapToInt(Integer::parseInt)
+            .toArray();
     }
 }
