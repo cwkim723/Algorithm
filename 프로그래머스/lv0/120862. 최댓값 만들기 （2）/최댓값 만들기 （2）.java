ebf19fcdf876b6916a1b8182c40c0f18c7d9b63b
Chaewon Kim <93105083+cwkim723@users.noreply.github.com>
2023-03-01 20:51:17 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.04 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+class Solution {
+    public long solution(int[] numbers) {
+        long answer = -1000000000;
+        for(int i = 0; i < numbers.length - 1; i++) {
+            for(int j = i+1; j < numbers.length; j++) {
+                if(numbers[i] * numbers[j] > answer) {
+                    answer = numbers[i] * numbers[j];
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
