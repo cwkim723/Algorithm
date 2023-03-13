d66466e9b572931bf15bc1329cfb79a5a8a73928
Chaewon Kim <93105083+cwkim723@users.noreply.github.com>
2023-03-14 00:14:54 +0900

    [level 0] Title: 공 던지기, Time: 0.01 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
@@ -1,20 +1,5 @@
 class Solution {
     public int solution(int[] numbers, int k) {
-        int answer = 0;
-        int index = 0;
-        int max = numbers.length; // 6
-        for(int count = 1; count <= k; count++) {
-            if(count == k) {
-                answer = numbers[index];
-                break;
-            }
-            
-            if(index + 2 < max) {
-                index = index + 2;
-            } else {
-                index = index + 2 - max;
-            }
-        }
-        return answer;
+        return numbers[((k - 1) * 2) % numbers.length];
     }
 }
