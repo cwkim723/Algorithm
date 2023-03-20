15aec15e30ace1dda651ea6e85276a4bb3a24672
Chaewon Kim <93105083+cwkim723@users.noreply.github.com>
2023-03-20 23:14:00 +0900

    Update 겹치는 선분의 길이.java

--- 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/lv0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -3,8 +3,6 @@ import java.util.*;
 class Solution {
     public int solution(int[][] lines) {
         int answer = 0;
-        Arrays.sort(lines, (o1,o2) -> (o1[0] == o2[0] ?  o1[1] - o2[1] : o1[0] - o2[0]));
-        System.out.println(Arrays.deepToString(lines));
         
         int[] range = new int[201];
         for(int[] line : lines) {;
