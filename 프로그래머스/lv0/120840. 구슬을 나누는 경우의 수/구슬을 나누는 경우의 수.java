047b776d9fb5a136531b98216d5bd59329f320b6
Chaewon Kim <93105083+cwkim723@users.noreply.github.com>
2023-03-26 14:06:32 +0900

    [level 0] Title: 구슬을 나누는 경우의 수, Time: 0.69 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
+++ 프로그래머스/lv0/120840. 구슬을 나누는 경우의 수/구슬을 나누는 경우의 수.java
@@ -1,26 +1,6 @@
-// class Solution {
-//     public long solution(int balls, int share) {
-//         long top = 1;
-//         for(int i = balls; i > share; i--) {
-//             top = top * i;
-//         }
-        
-//         long bottom = fact(balls - share);
-        
-//         return (top / (int)bottom);
-//     }
-    
-// 	long fact(long n) {
-// 		if (n <= 1) {
-// 	        return n;
-// 	    } else {
-// 	        return fact(n-1) * n;
-// 	    }
-// 	}
-// }
-
 import java.lang.*;
 import java.math.*;
+
 class Solution {
     public int solution(int balls, int share) {
         BigInteger top = BigInteger.ONE;
