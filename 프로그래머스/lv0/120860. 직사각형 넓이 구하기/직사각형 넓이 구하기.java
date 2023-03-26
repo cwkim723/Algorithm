3b2c9e28753a9841acce9ae17dea2ee0783f4ab1
Chaewon Kim <93105083+cwkim723@users.noreply.github.com>
2023-03-26 12:51:45 +0900

    Update 직사각형 넓이 구하기.java

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.java
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.java
@@ -5,7 +5,6 @@ class Solution {
         Arrays.sort(dots, (o1, o2) -> {
             return o1[0]-o2[0];
         });
-        System.out.println(Arrays.deepToString(dots));
         return Math.abs(dots[0][1] - dots[1][1]) * Math.abs(dots[0][0] - dots[2][0]);
     }
 }
